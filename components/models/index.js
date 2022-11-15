d8819ae373bd84b9d5f2167fbe387971dd1312fb
Dhieu1304 <d.hieu.13.04@gmail.com>
2022-11-15 22:23:28 +0700

    init data

--- components/models/index.js
+++ components/models/index.js
@@ -1,5 +1,56 @@
 const initModels = require('./init-models');
 const db = require('../../config/database.config');
-module.exports = {
-    models: initModels(db),
-}
+const models = initModels(db);
+
+(async () => {
+  try {
+    // init role
+    const roles = await models.role.findAll();
+    if (roles.length === 0) {
+      console.log('Init role data');
+      const data = [
+        { name: 'TEACHER' },
+        { name: 'STUDENT' },
+        { name: 'ADMIN' },
+      ];
+      await models.role.bulkCreate(data);
+    }
+
+    // init group user role
+    const groupUserRole = await models.group_user_role.findAll();
+    if (groupUserRole.length === 0) {
+      console.log('Init group user role data');
+      const data = [
+        { name: 'OWNER' },
+        { name: 'CO_OWNER' },
+        { name: 'MEMBER' },
+      ];
+      await models.group_user_role.bulkCreate(data);
+    }
+
+    // init user status
+    const userStatus = await models.user_status.findAll();
+    if (userStatus.length === 0) {
+      console.log('Init user status data');
+      const data = [
+        { name: 'ACTIVE' },
+        { name: 'IN_ACTIVE' },
+        { name: 'BLOCK' },
+        { name: 'DELETE' },
+      ];
+      await models.user_status.bulkCreate(data);
+    }
+
+    // init verify type
+    const verifyTypes = await models.verify_type.findAll();
+    if (verifyTypes.length === 0) {
+      console.log('Init verify type data');
+      const data = [{ name: 'RESET_PASSWORD' }, { name: 'VERIFY_MAIL' }];
+      await models.verify_type.bulkCreate(data);
+    }
+  } catch (e) {
+    console.error(e.message);
+  }
+})();
+
+module.exports = models;

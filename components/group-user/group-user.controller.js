f4ef84b1113a890dd9e4b1105c835cb8b44ada6e
Dhieu1304 <d.hieu.13.04@gmail.com>
2023-01-04 22:36:55 +0700

    fix change group role

--- components/group-user/group-user.controller.js
+++ components/group-user/group-user.controller.js
@@ -2,16 +2,17 @@ const { groupUserService } = require('../service.init');
 
 const changeRoleUser = async (req, res) => {
   const { userId, groupId, roleId } = req.body;
+  if (roleId === 1) {
+    return res.status(400).json({ status: false, message: 'Only change to co-owner or member' });
+  }
   const checkAdminInGroup = await groupUserService.findOneByUserIdAndGroupId(req.user.id, groupId);
-  console.log('checkAdminInGroup: ', checkAdminInGroup);
   if (!checkAdminInGroup && checkAdminInGroup?.group_user_role_id !== 1) {
     return res.status(400).json({ status: false, message: 'You do not have permission' });
   }
   const checkUserInGroup = await groupUserService.findOneByUserIdAndGroupId(userId, groupId);
   if (!checkUserInGroup) {
-    return res.status(400).json({ status: false, message: 'Invalid user in group' });
+    return res.status(400).json({ status: false, message: 'User is in group' });
   }
-
   const updateUserRole = await groupUserService.updateUserRoleInGroup(userId, groupId, roleId);
   if (updateUserRole) {
     return res.status(200).json({ status: true, message: 'Update successful' });
